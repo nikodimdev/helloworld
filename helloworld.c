e06295e21a4d780a69ba3a268732717480212247
Nikodimkim <nikodimkim@gmail.com>
2025-06-19 00:44:41 +0000

    Initial commit

---
+++ helloworld.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int 
+main ()
+{
+
+    printf("Hello World!");
+
+
+    return 0;
+}
+
+
+/*
+
+
+Задача из книги C Programming: A Modern Approach" (2nd Edition) by K.N. King
+Цель: написать программу которая выводит в консоль Hello World!
+
+
+*/
