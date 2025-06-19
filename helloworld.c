20cd93f0245ae08ad6dfb5a4ad73298e15e3b996
Nikodimkim <nikodimkim@gmail.com>
2025-06-19 00:59:27 +0000

    .

--- helloworld.c
+++ helloworld.c
@@ -11,12 +11,3 @@ main ()
     return 0;
 }
 
-
-/*
-
-
-Задача из книги C Programming: A Modern Approach" (2nd Edition) by K.N. King
-Цель: написать программу которая выводит в консоль Hello World!
-
-
-*/
